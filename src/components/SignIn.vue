351258c524149af3b2140dd0d921c1dc526e9168
zhaosi-kity <zhaosi@tradamis.org>
2025-10-30 18:49:42 +0800

    初始化 SignIn.vue 文件

---
+++ src/components/SignIn.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="sign-in">
+    <h2>签到</h2>
+    <button @click="handleSignIn">立即签到</button>
+    <p v-if="signedIn">签到成功！</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      signedIn: false
+    }
+  },
+  methods: {
+    handleSignIn() {
+      // 此处可接入后端接口
+      this.signedIn = true;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sign-in {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
