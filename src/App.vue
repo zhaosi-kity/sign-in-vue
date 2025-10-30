a9b8b6582167a3212f5056d351ac18161eb70803
zhaosi-kity <zhaosi@tradamis.org>
2025-10-30 18:49:35 +0800

    初始化 App.vue 文件

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <SignIn />
+  </div>
+</template>
+
+<script>
+import SignIn from './components/SignIn.vue'
+export default {
+  components: {
+    SignIn
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
